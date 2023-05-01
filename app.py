941961683145a89ed2134a62c10c21b32bd5448e
Douglas Maxwell <dsm1015@sru.edu>
2023-05-01 13:05:50 -0400

    final updates

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import openai
 import os
 import json
-import time
+from datetime import datetime
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 
@@ -32,13 +32,11 @@ def generate_response(character, character_info, chat_history):
     # Prepare the prompt for the OpenAI API
     prompt = f"User is having a conversation with {character}. {character} is from the novel {character_info['book']}.\nHere is some info about the character: {character_info['description']}\nThe character should respond in a manner consistent with their personality and background. Respond using only the tone, manner and vocabulary of the given character.\n\n"
 
-    print(prompt)
     for message in chat_history:
         prompt += f"{message['role']}: {message['message']}\n"
 
     prompt += f"{character}: "
 
-    print(prompt)
     # Call the OpenAI API
     response = openai.Completion.create(
         engine="text-davinci-003",
@@ -48,12 +46,19 @@ def generate_response(character, character_info, chat_history):
         stop=None,
         temperature=0.8,
         top_p=1
-    ) 
+    )
 
     # Extract the generated text from the response
     character_response = response.choices[0].text.strip()
-    #character_response = "test"
-    #time.sleep(2)
+
+    # Save the user message and character response to a file
+    with open("static/data/conversation_history.txt", "a") as f:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        f.write(f"Time: {timestamp}\n")
+        last_user_message = chat_history[-1]['message']
+        f.write(f"User: {last_user_message}\n")
+        f.write(f"{character}: {character_response}\n")
+        f.write(os.linesep)  # Add a line separator to distinguish conversations
 
     return character_response
 
